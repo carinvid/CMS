f950c6563722b29b81d8437c61a9d921a067ec59
Carolina Reedy <carinvid@gmail.com>
2021-08-06 14:09:19 -0600

    add connection to sql

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require("mysql2");
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -15,6 +16,23 @@ app.get("/", (req, res) => {
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
+});
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // Your MySQL username,
+    user: "root",
+    // Your MySQL password
+    password: "Americo01",
+    database: "cms",
+  },
+  console.log("Connected to the cms database.")
+);
+
+db.query(`SELECT * FROM employees`, (err, rows) => {
+  console.log(rows);
 });
 
 app.listen(PORT, () => {
