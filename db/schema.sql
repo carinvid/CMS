fedd0f3d66b3cca16bfc967bec6bee7f575c6b40
Carolina Reedy <carinvid@gmail.com>
2021-08-07 12:45:34 -0600

    add table

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,20 @@ CREATE TABLE employees (
   last_name VARCHAR(30) NOT NULL,
   title VARCHAR(30) NOT NULL,
   department VARCHAR(30) NOT NULL,
-  salary DECIMAL (5,2) NOT NULL,
+  salary DECIMAL (10,2) NOT NULL,
   manager VARCHAR(30) NOT NULL,
+);
+CREATE TABLE department (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INTEGER,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
