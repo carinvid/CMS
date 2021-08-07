092e3becf407750d7d24663391a58948fd24456d
Carolina Reedy <carinvid@gmail.com>
2021-08-06 19:03:12 -0600

    add connection

--- server.js
+++ server.js
@@ -2,14 +2,31 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mysql = require("mysql2");
+const inputCheck = require("./utils/inputCheck");
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Hello World",
+// app.get("/", (req, res) => {
+//   res.json({
+//     message: "Hello World",
+//   });
+// });
+
+// Get all employees
+app.get("/api/employees", (req, res) => {
+  const sql = `SELECT * FROM employees`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
   });
 });
 
@@ -30,10 +47,6 @@ const db = mysql.createConnection(
   },
   console.log("Connected to the cms database.")
 );
-
-db.query(`SELECT * FROM employees`, (err, rows) => {
-  console.log(rows);
-});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
